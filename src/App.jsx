f8e8131a0947683faf425fb904575df244afcec1
ashleysaintlouis <saintlouisashlhey@gmail.com>
2025-03-20 09:01:42 -0300

    Login, Register commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
 
 function App() {
 
@@ -14,10 +17,12 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
 
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
+import styles from './Navbar.module.css'
 import { NavLink } from 'react-router-dom'
+
 
 const Navbar = () => {
 
   return (
-    <nav>
-        <NavLink to="/">
+    <nav className={styles.navbar}>
+        <NavLink className={styles.brand} to="/">
             Mini <span>Blog</span>
-        </NavLink>
-        <ul>
+        </NavLink >
+        <ul className={styles.links_list}>
             <li>
-                <NavLink to="/">
+                <NavLink className={({isActive}) => (isActive ? styles.active : "")}  to="/">
                     Inicio
-                </NavLink>
+                </NavLink >
             </li>
             <li>
-                <NavLink to="/about">
+                <NavLink className={({isActive}) => (isActive ? styles.active : "")}  to="/login">
+                    Entrar
+                </NavLink >
+            </li>
+            <li>
+                <NavLink className={({isActive}) => (isActive ? styles.active : "")}  to="/register">
+                    Cadastrar
+                </NavLink >
+            </li>
+            <li>
+                <NavLink className={({isActive}) => (isActive ? styles.active : "")} to="/about">
                     Sobre
-                </NavLink>
+                </NavLink >
             </li>
         </ul>
     </nav>
