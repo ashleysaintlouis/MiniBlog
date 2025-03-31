3ea14ad8c28b0847f5fb543a4a44d38d47abe8e1
ashleysaintlouis <saintlouisashlhey@gmail.com>
2025-03-31 08:55:32 -0300

    commit useFetchDocuments

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
 
 //hooks
-import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 // components
+import PostDetail from '../../components/postDetail/PostDetail'
 
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([])
-
+  const { documents: posts, loading} = useFetchDocuments("posts")
+  
   const handleSearchPosts = (e) => {
     e.preventDefault();
 
@@ -22,7 +24,7 @@ const Home = () => {
     <div className={styles.home}>
       <h1>Veja os nossos posts mais recentes</h1>
 
-      <form onSubmit={handleSearchPosts}>
+      <form onSubmit={handleSearchPosts} className={styles.search_form}>
         <input 
         type="text" 
         placeholder='Ou busque por tags...'
@@ -33,8 +35,12 @@ const Home = () => {
       </form>
 
       <div>
-        <h1>Posts...</h1>
-        {!posts && posts.length === 0 && (
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => 
+      
+          <PostDetail key={post.id} post={post} />
+        )}
+        {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrado posts</p>
             <Link to="/posts/create">Criar primeiro post</Link>
